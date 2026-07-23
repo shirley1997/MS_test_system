a51766436d1e62c8e1bdbca68bda72359c376e36
Xueting Huang <xueting.huang404@gmail.com>
2026-07-23 18:41:06 +0200

    remove pom.xml from gitrepo to reduce experiment confusion.

---
+++ automation_process/config_generator/generate_pom_xml.py
@@ -0,0 +1,4 @@
+# this script is used to generate pom.xml
+# pom.xml contains variable B1: package manager configuration -> registry URL configuration
+# AND dependency version specifier
+# so in java service CI pipeline, there is no need to produce settings.xml for package manager configuration
