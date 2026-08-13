a2391697f449d132698e3143843399f2f48250bf
Xueting Huang <xueting.huang404@gmail.com>
2026-08-13 15:17:12 +0200

    start implementing central automated pipeline for automatically conducting the sub-RQ1 experiment

---
+++ automation_process/central_automated_pipeline/central_automated_pipeline.py
@@ -0,0 +1,5 @@
+# this is the file of central_automated_pipeline
+# the central automated pipeline is divided into several phases, each phase is implemented in order 
+# so the function of the whole pipeline can be determined and checked more clean
+# and only implement next phase when the current phase is tested and function properly
+# this central automated pipeline is used to automatically conduct the experiment for sub-RQ1
